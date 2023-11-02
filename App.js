cf8375701e238f39392570c1491bd871878fe0ec
Rob Caldecott <robert.caldecott@gmail.com>
2023-11-02 13:11:45 +0000

    Tweaks

--- App.js
+++ App.js
@@ -11,7 +11,6 @@ function LoginButton(props) {
       className={
         "h-12 flex-row items-center justify-between rounded-full border-2 border-black px-4 bg-yellow-200 active:bg-slate-500"
       }
-      disabled={props.disabled}
       onPress={props.onPress}
     >
       <Icon height={28} width={28} />
@@ -23,10 +22,25 @@ function LoginButton(props) {
 export default function App() {
   return (
     <View className="flex-1 bg-white items-center justify-center gap-8">
-      <Pressable className="active:bg-gray-600 p-10 bg-yellow-100">
+      <Text>Pressable with broken styles</Text>
+      <Pressable className="active:bg-gray-600 p-10 bg-yellow-300">
         <Text className="text-red-500 text-2xl">Pressable</Text>
       </Pressable>
+
+      <Text>Sample button lifted from our app</Text>
       <LoginButton icon={GoogleLogo}>Continue with Google</LoginButton>
+
+      <Text>View styles working!</Text>
+      <View className="flex-row gap-4 items-center justify-center h-12 rounded-full border-2 border-black px-4 bg-yellow-200">
+        <GoogleLogo height={28} width={28} />
+        <Text>Contine with Google</Text>
+      </View>
+
+      <Text>View with active: styles not working</Text>
+      <View className="flex-row gap-4 items-center justify-center h-12 rounded-full border-2 border-black px-4 bg-yellow-200 active:bg-yellow-500">
+        <GoogleLogo height={28} width={28} />
+        <Text>Contine with Google</Text>
+      </View>
     </View>
   );
 }
